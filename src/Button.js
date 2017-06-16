bce5d271d69e1fdc3026de1637eb61b6c3fe0d6b
chanashol <chanashol@miraimedia.co.th>
2017-06-16 09:42:40 +0700

    start project dta-mirai

---
+++ src/Button.js
@@ -0,0 +1,10 @@
+import React, { Component } form 'react';
+
+class Button extends Component {
+	render() {
+	//...
+		return <div className="Button" />;
+	}
+}
+
+export default Button;
---
+++ src/DangerButton.js
@@ -0,0 +1,10 @@
+import React, { Component } form 'react';
+import Button form './Button';
+
+class DangerButton extends Component {
+	render() {
+		return <Button color="red" />;
+	}
+}
+
+export default DangerButton;
